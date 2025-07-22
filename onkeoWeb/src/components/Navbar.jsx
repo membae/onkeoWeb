8c264181dafa0818e5b57c3e60b7716858718b01
memba <membaphilip1@gmail.com>
2025-07-22 05:36:35 +0300

    Navbar

--- onkeoWeb/src/components/Navbar.jsx
+++ onkeoWeb/src/components/Navbar.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-      {/* Logo */}
-      <Link to="/" className="text-2xl font-bold text-blue-600">
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
         OnkeoWeb
       </Link>
 
       {/* Navigation Links */}
-      <div className="space-x-6">
-        <Link to="/about" className="text-gray-700 hover:text-blue-600">
+      <div className="navbar-links">
+        <Link to="/about" className="navbar-link">
           About
         </Link>
-        <Link to="/services" className="text-gray-700 hover:text-blue-600">
+        <Link to="/services" className="navbar-link">
           Services
         </Link>
-        <Link to="/contact" className="text-gray-700 hover:text-blue-600">
+        <Link to="/contact" className="navbar-link">
           Contact
+        </Link>
+        <Link to="/testimonials" className='navbar-link'>
+          Testimonials
         </Link>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
