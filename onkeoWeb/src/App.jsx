76bea9dc16ecf4ee20b5b8f7b2f0e7d925bf9698
memba <membaphilip1@gmail.com>
2025-08-17 17:39:27 +0300

    started on services

--- onkeoWeb/src/App.jsx
+++ onkeoWeb/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About'
+import Services from './pages/Services'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='about' element={<About/>}/>
+          <Route path='services' element={<Services/>}/>
         </Routes>
       </BrowserRouter>
     </div>
