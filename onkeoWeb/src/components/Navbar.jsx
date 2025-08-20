16941ec14e9f9a490ad06ba26f6905f510154cb1
memba <membaphilip1@gmail.com>
2025-08-20 19:44:05 +0300

    navbar

--- onkeoWeb/src/components/Navbar.jsx
+++ onkeoWeb/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css';
 
 function Navbar() {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
@@ -12,38 +13,48 @@ function Navbar() {
           OnkeoWeb
         </Link>
 
+        {/* Hamburger icon */}
+        <div 
+          className="hamburger" 
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
         {/* Navigation Links */}
-        <div className="navbar-links">
-          <Link to="/" className="navbar-link">
+        <div className={`navbar-links ${isMobileMenuOpen ? "active" : ""}`}>
+          <Link to="/" className="navbar-link" onClick={() => setIsMobileMenuOpen(false)}>
             Home
           </Link>
-          <Link to="/about" className="navbar-link">
+          <Link to="/about" className="navbar-link" onClick={() => setIsMobileMenuOpen(false)}>
             About
           </Link>
 
           {/* Dropdown for Services */}
           <div
             className="navbar-link dropdown"
-            onMouseEnter={() => setShowDropdown(true)}
-            onMouseLeave={() => setShowDropdown(false)}
+            onMouseEnter={() => !isMobileMenuOpen && setShowDropdown(true)}
+            onMouseLeave={() => !isMobileMenuOpen && setShowDropdown(false)}
           >
             <span>Services</span>
-            {showDropdown && (
+            {(showDropdown || isMobileMenuOpen) && (
               <div className="dropdown-menu">
-                <Link to="/services/architecture" className="dropdown-item">
+                <Link to="/services/architecture" className="dropdown-item" onClick={() => setIsMobileMenuOpen(false)}>
                   Design your property
                 </Link>
-                <Link to="/services/build" className="dropdown-item">
+                <Link to="/services/build" className="dropdown-item" onClick={() => setIsMobileMenuOpen(false)}>
                   Build your property
                 </Link>
               </div>
             )}
           </div>
 
-          <Link to="/contact" className="navbar-link">
+          <Link to="/contact" className="navbar-link" onClick={() => setIsMobileMenuOpen(false)}>
             Contact
           </Link>
-          <Link to="/testimonials" className="navbar-link">
+          <Link to="/testimonials" className="navbar-link" onClick={() => setIsMobileMenuOpen(false)}>
             Testimonials
           </Link>
         </div>
